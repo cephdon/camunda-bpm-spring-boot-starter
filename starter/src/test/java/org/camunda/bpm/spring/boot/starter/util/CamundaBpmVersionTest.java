f967a6c62470fe791e4a2c46f306c733aa1d2e3c
Svetlana Dorokhova <svetlana.dorokhova@camunda.com>
2017-09-06 14:26:44 +0200

    fix(spring-boot): cleanup webapp starter + fix test

--- starter/src/test/java/org/camunda/bpm/spring/boot/starter/util/CamundaBpmVersionTest.java
+++ starter/src/test/java/org/camunda/bpm/spring/boot/starter/util/CamundaBpmVersionTest.java
@@ -10,7 +10,7 @@ import static org.mockito.Mockito.when;
 
 public class CamundaBpmVersionTest {
 
-  private static final String CURRENT_VERSION = "7.7.0";
+  private static final String CURRENT_VERSION = "7.8.0";
 
   public static CamundaBpmVersion camundaBpmVersion(final String version) {
     final Package pkg = mock(Package.class);
@@ -22,12 +22,13 @@ public class CamundaBpmVersionTest {
   public void current_version() {
     final CamundaBpmVersion version =  new CamundaBpmVersion();
     assertThat(version.isEnterprise()).isFalse();
-    assertThat(version.get()).isEqualTo(CURRENT_VERSION);
+    assertThat(version.get()).startsWith(CURRENT_VERSION);
 
     final PropertiesPropertySource source = version.getPropertiesPropertySource();
     assertThat(source.getName()).isEqualTo("CamundaBpmVersion");
-    assertThat(source.getProperty(key(CamundaBpmVersion.VERSION))).isEqualTo(CURRENT_VERSION);
-    assertThat(source.getProperty(key(CamundaBpmVersion.FORMATTED_VERSION))).isEqualTo("(v" + CURRENT_VERSION + ")");
+    final String versionFromPropertiesSource = (String) source.getProperty(key(CamundaBpmVersion.VERSION));
+    assertThat(versionFromPropertiesSource).startsWith(CURRENT_VERSION);
+    assertThat(source.getProperty(key(CamundaBpmVersion.FORMATTED_VERSION))).isEqualTo("(v" + versionFromPropertiesSource + ")");
     assertThat(source.getProperty(key(CamundaBpmVersion.IS_ENTERPRISE))).isEqualTo(Boolean.FALSE);
   }
 
